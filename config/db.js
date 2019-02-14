ced8d9e21bacf5df853fea839c58b804a9add401
David Jeyachandran <davidjeyachandran@gmail.com>
2019-02-14 11:17:41 -0500

    Add db config and package

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
     dbuser: "david",
     dbpassword: "1password",
-	url : "mongodb://" + dbuser + ":" + dbpassword + "@ds123625.mlab.com:23625/dj-quotes"
+	url : "mongodb://" + this.dbuser + ":" + this.dbpassword + "@ds123625.mlab.com:23625/dj-quotes"
 }
